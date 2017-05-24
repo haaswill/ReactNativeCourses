cc6bfc5853deccae05738dfeddf21d6707f1978f
Willian <haaswillian@gmail.com>
2017-05-23 23:14:00 -0300

    Finished the app

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBaPyJI6YiFQeCdkdlbxguH3UKrvCyWTEk',
@@ -14,13 +16,42 @@ class App extends Component {
       storageBucket: 'auth-cb856.appspot.com',
       messagingSenderId: '146135341568'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <CardSection>
+            <Spinner size="large" />
+          </CardSection>
+        );
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
--- auth/src/components/common/Spinner.js
+++ auth/src/components/common/Spinner.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View, ActivityIndicator } from 'react-native';
 
-const Spinner = ({ size }) => (
-  <View style={styles.spinnerStyle}>
-    <ActivityIndicator size={size || 'large'} />
-  </View>
-);
+const Spinner = ({ size }) => {
+  return (
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator size={size || 'large'} />
+    </View>
+  );
+};
+
 
 const styles = {
   spinnerStyle: {
