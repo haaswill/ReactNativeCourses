aca0da51ccb57f2cfa5c11da4ca5b714ffaacbab
Willian <haaswillian@gmail.com>
2017-05-24 22:15:23 -0300

    Finished 102nd class

--- tech_stack/src/reducers/index.js
+++ tech_stack/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
 // Makes multiple reducers work together, assignin them to a key
 // { libraries: [] }
 export default combineReducers({
-  libraries: LibraryReducer
+  libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer
 });
