7f7aa08e2cbb849ba5b639acfc50f88ee28b24ed
Willian <haaswillian@gmail.com>
2017-06-02 16:44:08 -0300

    Added navigation and created welcome screen

--- jobs/main.js
+++ jobs/main.js
@@ -1,12 +1,36 @@
 import Expo from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import ReviewScreen from './screens/ReviewScreen';
 
 class App extends React.Component {
   render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        })
+      }
+    });
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <MainNavigator />
       </View>
     );
   }
@@ -16,7 +40,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
---
+++ jobs/screens/ReviewScreen.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text, Platform } from 'react-native';
+import { Button } from 'react-native-elements';
+
+class ReviewScreen extends Component {
+  // static make it a property of the class and not the instance
+  // React Navigation uses navigationOptions as configuration for the navigator
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Review Jobs',
+      headerRight: (
+        <Button
+          title="Settings"
+          onPress={() => navigation.navigate('settings')}
+          backgroundColor='rgba(0,0,0,0)'
+          color='rgba(0,122,255,1)'
+        />
+      ),
+      style: {
+        marginTop: Platform.OS === 'android' ? 24 : 0
+      }
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>ReviewScreen</Text>
+      </View>
+    );
+  }
+}
+
+export default ReviewScreen;
